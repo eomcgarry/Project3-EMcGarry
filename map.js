f607f654e541209f923872eda81b9b204bf617d0
eomcgarry <46875404+eomcgarry@users.noreply.github.com>
2019-04-16 23:10:42 -0500

    Add JS body
    Add body for map js page

--- map.js
+++ map.js
@@ -1,4 +1,16 @@
 function initMap() {
+	// Initialize stadium variable
 	var campNou = {lat: 41.380896, lng: 2.1228198};
 	
+	// Initialize map variable with close zoom
+	var map = new google.maps.Map(document.getElementById('map'), {center: campNou, zoom: 18, mapTypeId: 'satellite'});
+	
+	// Initialize map marker
+	var marker = new google.maps.Marker({position: campNou, map: map, google.maps.Animation.DROP});
+	
+	// Initialize info for marker
+	var info = new google.maps.InfoWindow ({content: 'Camp Nou'})
+	
+	// Initialize variable for click listener on marker
+	marker.addListener('click', function() info.open(map, marker);
 }
