57776d03826692d34ca6f2410ce35e36f837702a
eomcgarry <46875404+eomcgarry@users.noreply.github.com>
2019-04-30 23:16:20 -0500

    Fixing Map

--- map.js
+++ map.js
@@ -1,24 +1,19 @@
 function initMap() {
 	// Initialize stadium variable
-	var campNou = {lat: 41.380896, lng: 2.1228198};
+	// var campNou = {lat: 41.380896, lng: 2.1228198};
 	
 	// Initialize map variable with close zoom
-	var map = new google.maps.Map(document.getElementById('map'), {
-		center: campNou,
-		zoom: 18,
-		mapTypeId: 'satellite'
+	var gMap = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 41.380896, lng: 2.1228198} campNou,
+		zoom: 15,
+		mapTypeId: google.maps.MapTypeId.SATELLITE
 		});
 	
 	// Initialize map marker
 	var marker = new google.maps.Marker({
-		position: campNou,
-		map: map,
-		animation: google.maps.Animation.DROP
+		position: {lat: 41.380896, lng: 2.1228198},
+		map: gMap,
+		 animation: google.maps.Animation.DROP
 		});
 	
-	// Initialize info for marker
-	var info = new google.maps.InfoWindow ({content: 'Camp Nou'})
-	
-	// Initialize variable for click listener on marker
-	marker.addListener('click', function() {info.open(map, marker)});
 }
