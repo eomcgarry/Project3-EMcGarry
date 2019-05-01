60bdabae621d6a04f5827614788784eeb9e1e7e2
eomcgarry <46875404+eomcgarry@users.noreply.github.com>
2019-04-30 23:19:59 -0500

    Fixed map
    Finally fixed map,removed position syntax error

--- map.js
+++ map.js
@@ -1,19 +1,19 @@
 function initMap() {
 	// Initialize stadium variable
-	// var campNou = {lat: 41.380896, lng: 2.1228198};
+	var campNou = {lat: 41.380896, lng: 2.1228198};
 	
 	// Initialize map variable with close zoom
 	var gMap = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 41.380896, lng: 2.1228198} campNou,
-		zoom: 15,
+		center: campNou,
+		zoom: 18,
 		mapTypeId: google.maps.MapTypeId.SATELLITE
 		});
 	
 	// Initialize map marker
 	var marker = new google.maps.Marker({
-		position: {lat: 41.380896, lng: 2.1228198},
+		position: campNou,
 		map: gMap,
-		 animation: google.maps.Animation.DROP
+		animation: google.maps.Animation.DROP
 		});
 	
 }
